7da30ddfa7e45bcc81a4755f03d82f5f4fd5f064
yugo <yugosakamoto0717@gmail.com>
2025-04-21 16:13:05 -0300

    [feat] Includes inputs on Home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function Home() {
+  return (
+    <div className="Container">
+      <h1 className="Title">Login</h1>
+      <form>
+        <input type="text" />
+        <input type="password" />
+      </form>
+    </div>
+  );
+}

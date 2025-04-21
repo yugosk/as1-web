b85c821f0558be5fc3523d822a26a4af4136019c
yugo <yugosakamoto0717@gmail.com>
2025-04-21 16:22:32 -0300

    [feat] Changes App component to show Home component and includes input for login

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,36 @@
 import React, { useState } from "react";
 
 function Home() {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  let texto = "";
+
   return (
     <div className="Container">
       <h1 className="Title">Login</h1>
       <form>
-        <input type="text" />
-        <input type="password" />
+        <input
+          type="text"
+          id="email"
+          value={email}
+          required
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          id="password"
+          value={password}
+          placeholder="Senha"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Acessar</button>
       </form>
+
+      <p className="SuccessText">{texto}</p>
     </div>
   );
 }
+
+export default Home;
